bdffd4a676597f9ebe15d13724b0c18c85a8a90f
sayedmohamed-saqib <saqibmohamed02@gmail.com>
2026-04-21 09:57:59 +0530

    Added Files related to basic annotations in core java

---
+++ Core_Java/Core_Java_Annotations.java
@@ -0,0 +1,21 @@
+class I {
+    public void showMethodThatBelongsToI(){
+        System.out.println("In I Show");
+    }
+}
+
+class J extends I {
+
+    @Override
+    public void showMethodThatBelongstoI(){
+        System.out.println("In J show");
+    }
+}
+
+public class Core_Java_Annotations {
+
+    public static void main(String[] args) {
+        J obj = new J();
+        obj.showMethodThatBelongsToI();
+    }
+}
