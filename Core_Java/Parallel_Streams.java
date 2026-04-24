ea47f9ffc4d40edfa44058613f738099bc8b8b83
sayedmohamed-saqib <saqibmohamed02@gmail.com>
2026-04-24 12:55:56 +0530

    Added concept files relatd to parallelStreams

---
+++ Core_Java/Parallel_Streams.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Parallel_Streams {
+    
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>(10000);
+        Random rand = new Random();
+
+        for(int i = 1; i <= 10000; i++) {
+            nums.add(rand.nextInt(100));
+        }
+
+        long startStream = System.currentTimeMillis();
+        int sum1 = nums.stream().map(n -> n * 2).reduce(0, (c,e) -> c + e);
+        long endStream = System.currentTimeMillis();
+
+        long startPara = System.currentTimeMillis();
+        int sum2 = nums.parallelStream().map(n -> n * 2).mapToInt(i -> i).sum();
+        long endPara = System.currentTimeMillis();
+
+        long res1 = (endStream - startStream);
+        long res2 = (endPara - startPara);
+
+        System.out.println("Sum1: " + sum1 + " " + "sum2: " + sum2 );
+        System.out.println("Time taken for operation 1 is: " + res1 + " ms, Time taken for operation 2: " + res2 + "ms");
+    }
+
+}
