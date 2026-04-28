f653fe0d7a4594905cd07144c4576689ee1ef5a7
sayedmohamed-saqib <saqibmohamed02@gmail.com>
2026-04-28 07:16:21 +0530

    Added concept files related to LVTI var

---
+++ Core_Java/LVTI.java
@@ -0,0 +1,14 @@
+public class LVTI {
+    public static void main(String[] args) {
+        
+        int age = 10;
+        var age2 = 12;
+
+        System.out.println(age2);
+
+        var num = new String[] {"Saqib", "Pratibha", "Nupoor"};
+        for(int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
