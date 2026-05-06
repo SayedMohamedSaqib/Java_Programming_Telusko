e42936d5304cf67eadd752a149b1a8f57a18a6ea
sayedmohamed-saqib <saqibmohamed02@gmail.com>
2026-05-06 11:00:44 +0530

    Added a junit test integrating surefire plugin used for CI/CD pipelines

---
+++ JUnit5/junit_03/src/test/java/Test_03.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Test_03 {
+
+    @Test
+    void test() {
+        assertEquals(6, 6);
+    }
+
+    @Test
+    void test_2() {
+        assertEquals(10,10);
+    }
+}
