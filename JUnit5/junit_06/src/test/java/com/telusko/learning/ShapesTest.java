4428cb7bd8ac208ebfccb16d7a27749b67159411
sayedmohamed-saqib <saqibmohamed02@gmail.com>
2026-05-07 10:00:52 +0530

    Updated file with @TestInstance annotation

--- JUnit5/junit_06/src/test/java/com/telusko/learning/ShapesTest.java
+++ JUnit5/junit_06/src/test/java/com/telusko/learning/ShapesTest.java
@@ -5,7 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS);
 class ShapesTest {
 
     Shapes shape;
