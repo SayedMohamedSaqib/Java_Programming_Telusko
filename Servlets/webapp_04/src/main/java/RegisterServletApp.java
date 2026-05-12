e64125aa3e49b12ffb6195678bbe079468592376
sayedmohamed-saqib <saqibmohamed02@gmail.com>
2026-05-12 10:43:55 +0530

    Added a new webapp with usage of jsp files, redirection from servlet

---
+++ Servlets/webapp_04/src/main/java/RegisterServletApp.java
@@ -0,0 +1,21 @@
+import java.io.PrintWriter;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/RegisterServletApp")
+public class RegisterServletApp extends HttpServlet {
+
+    protected void doPost(
+            jakarta.servlet.http.HttpServletRequest req,
+            jakarta.servlet.http.HttpServletResponse resp)
+            throws jakarta.servlet.ServletException, java.io.IOException {
+
+        String name = req.getParameter("uname");
+        String city = req.getParameter("ucity");
+
+        PrintWriter writer = resp.getWriter();
+
+        resp.sendRedirect(req.getContextPath() + "/success.jsp");
+    }
+}
